0e347d499cffd731e0a8107ed86b8003b999bf19
Scott Bedard <scottbedard90@gmail.com>
2021-09-26 02:04:45 -0500

    fix spacing

--- src/MapCapitalize.d.ts
+++ src/MapCapitalize.d.ts
@@ -4,9 +4,9 @@
  * @example
  * type Capitalized = MapLowercase<['foo', 'bar']> // ['Foo', 'Bar']
  */
- export type MapCapitalize<
- T extends string[],
- Acc extends string[] = [],
+export type MapCapitalize<
+  T extends string[],
+  Acc extends string[] = [],
 > = T extends [infer Head, ...infer Tail]
- ? MapCapitalize<Tail, [...Acc, Capitalize<Head>]>
- : Acc
+  ? MapCapitalize<Tail, [...Acc, Capitalize<Head>]>
+  : Acc
