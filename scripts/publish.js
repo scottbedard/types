db633208ab763b1d5f2f3698720557d39dc8c299
Scott Bedard <scottbedard90@gmail.com>
2021-11-12 20:38:04 -0600

    commit root index to source control

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,15 +3,12 @@ const fs = require('fs')
 const path = require('path')
 const pkg = require('../package.json')
 
-const rootIndex = path.resolve(__dirname, '../index.ts')
 const srcIndex = path.resolve(__dirname, '../src/index.ts')
 
 async function cli() {
   const src = String(fs.readFileSync(srcIndex)).replace('x.y.z', pkg.version)
 
   fs.writeFileSync(srcIndex, src)
-
-  fs.writeFileSync(rootIndex, `export * from './src/index'`)
 
   exec('npm publish')
 }
